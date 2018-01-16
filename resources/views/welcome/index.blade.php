d0cb4a651e5cb48d4a5a2c173ce99fb60edcfa6b
yachu <acercarol999@gmail.com>
2018-01-16 16:47:19 +0800

    Practice using template extend system

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,95 +1,26 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Welcome to HackerPair</title>
+@section('title')
+Welcome to HackerPair
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('content')
+    <div class="title m-b-md">
+        Hi {{ $name or 'Guest' }}, Welcome to HackerPair
+    </div>
+    <div class="links">
+        <a href="{{ route('events.show', ['id' => 42]) }}">Events</a>
+        <a href="https://laravel.com/docs">Documentation</a>
+        <a href="https://laracasts.com">Laracasts</a>
+        <a href="https://laravel-news.com">News</a>
+        <a href="https://forge.laravel.com">Forge</a>
+        <a href="https://github.com/laravel/laravel">GitHub</a>
+    </div>
+@endsection
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Hi {{ $name or 'Guest' }}, Welcome to HackerPair
-                </div>
-                <div class="links">
-                    <a href="{{ route('events.show', ['id' => 42]) }}">Events</a>
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('advertisement')
+    @parent
+    <p>
+        HackerPair members always get 10% off at Tron Cafe!
+    </p>
+@endsection
